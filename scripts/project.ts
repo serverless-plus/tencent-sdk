a8435fc0b9b8edf58075f9ba670b3dbe46b80bf0
yugasun <yuga_sun@163.com>
2019-12-10 19:03:02 +0800

    fix: rm tsc build

--- scripts/project.ts
+++ scripts/project.ts
@@ -20,12 +20,8 @@ export default {
       const pkgScopedName = p;
       const pkgDist = path.join(pkgPath, 'dist');
 
-      const pkgUmd = path.join(pkgDist, 'index.umd.js');
+      const pkgUmd = path.join(pkgDist, 'index.js');
       const pkgEsm = path.join(pkgDist, 'index.esm.js');
-      const pkgSystem = path.join(pkgDist, 'index.system.js');
-      const pkgAmd = path.join(pkgDist, 'index.amd.js');
-      const pkgCjs = path.join(pkgDist, 'index.cjs.js');
-      const pkgIife = path.join(pkgDist, 'index.js');
 
       return {
         name: pkgName,
